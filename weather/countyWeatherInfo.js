1006f19f9b6c85a12a89e8db705ba3d46cde746e
xCATalystx <66053700+xCATalystx@users.noreply.github.com>
2021-03-15 17:10:52 +0800

    Add files via upload

--- weather/countyWeatherInfo.js
+++ weather/countyWeatherInfo.js
@@ -1,19 +1,21 @@
-const countyData = [];
+const countyInfo = [];
+const taoyuanInfo = [];
 
 $.get("https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWB-4D24F5BE-73F7-485A-94A0-AB7B5E86D535&format=JSON",
 function(data){
-  const countyInfo = data["records"].location;
+  const countyData = data["records"].location;
 
 
   $.getJSON("twMapId.json", function(data2){
     const countyId = data2.map(item => Object.values(item)[1]);
+    const countyValue = data2.map(item => Object.values(item)[2]);
 
     //整理縣市資料
 
-    for (i = 0; i < countyInfo.length; i++) {
-      const wxs = countyInfo[i].weatherElement;
-      const weatherInfo = {
-        county: countyInfo[i].locationName,
+    for (i = 0; i < countyData.length; i++) {
+      const wxs = countyData[i].weatherElement;
+      const weatherData = {
+        county: countyData[i].locationName,
         wx: wxs[0].time[0].parameter.parameterName,
         wxV: wxs[0].time[0].parameter.parameterValue,
         pop: wxs[1].time[0].parameter.parameterName,
@@ -21,23 +23,23 @@ function(data){
         maxT: wxs[4].time[0].parameter.parameterName,
         ci: wxs[3].time[0].parameter.parameterName,
         id: countyId[i],
-        time: wxs[0].time[0].startTime,
+        value: countyValue[i],
+        date: ((wxs[0].time[0].startTime).split(' ')[0]).split('-'),
       };
       
-      countyData.push(weatherInfo);
+      countyInfo.push(weatherData);
     };
-    
 
-    //將值寫入weatherData和infoDate
+    // 把資料寫入weatherData和infoDate
 
-    const weatherData = function(countyData){
+    const weatherInfo = function(countyInfo){
       let htmlStr = "";
       let timeStr = "";
 
-      for (idx in countyData) {
-        const { county, wx, wxV, minT, maxT, pop, id, time } = countyData[idx];
+      for (idx in countyInfo) {
+        const { county, wx, wxV, minT, maxT, pop, id, value, date } = countyInfo[idx];
         htmlStr += `
-        <a href="javascript:void(0)" title="${county}：${wx}" class="iconZone" name="${id}">
+        <a href="javascript:void(0)" title="${county}：${wx}" class="iconZone" name="${id}" value="${value}">
           <div>
             <span class="city">${county}</span>
             <span>${wx}</span>
@@ -46,10 +48,8 @@ function(data){
           </div>
           <img src="images/weatherImg/${wxV}.svg" alt="${county}：${wx}" title="${county}：${wx}">
         </a>`;
-              
-        const date = time.split(' ');
         
-        timeStr = `<p><i class="far fa-calendar-alt"></i> ${date[0]}</p>`;
+        timeStr = `<p><i class="far fa-calendar-alt"></i> ${date}</p>`;
       }
       $(".weatherData").empty();
       $(".weatherData").html(htmlStr);
@@ -57,22 +57,138 @@ function(data){
       $(".infoDate").empty();
       $(".infoDate").html(timeStr);
     };
-    weatherData(countyData);
+    weatherInfo(countyInfo);
+
+    // 把資料寫入countyWxPage
+    const countyWeather = function(countyInfo, countyValue){
+      let wxPageStr = "";
+
+      for (idx in countyInfo) {
+        const { county, wx, wxV, minT, maxT, pop, ci, date } = countyInfo[countyValue];
+
+        wxPageStr = `
+          <div class="wxPage1">
+            <div class="wxPageLocation">
+              <span><i class="fas fa-map-marker-alt"></i> ${county}</span>
+            </div>
+            <div class="wxPageImg">
+              <img src="images/weatherImg/${wxV}.svg" alt="${wx}">
+            </div>
+          </div>
+          <div class="wxPage2">
+            <div>
+              <span> ${date[1]}/${date[2]}</span>
+            </div>
+            <ul>
+              <li><span>${wx}</span></li>
+              <li><span><i class="fas fa-temperature-low"></i> ${minT} ~ ${maxT}</span><i>˚C</i></li>
+              <li><span><i class="fas fa-umbrella"></i> ${pop}</span><i>%</i></li>
+              <li><span>${ci}</span></li>
+            </ul>
+          </div>`;
+      };
+      $(".countyWxPage").empty();
+      $(".countyWxPage").html(wxPageStr);
+    };
+
+    $("#countySelectBar").on("change", function(){
+      const countyValue = $(this).val();
+      countyWeather(countyInfo, countyValue);
+    });
+    $('.weatherData').on('click', 'img', function(e){
+      const countyValue = $(e.target).parent('a').attr('value');
+      countyWeather(countyInfo, countyValue);
+    });
+    $('.twMap a').on('click', function(e){
+      const countyValue = $(e.target).parent('g').attr('name');
+      countyWeather(countyInfo, countyValue);
+    });
+
+    // 預設氣象(桃園市)
+
+    const taoyuanData = {
+      county: countyData[13].locationName,
+        wx: countyData[13].weatherElement[0].time[0].parameter.parameterName,
+        wxV: countyData[13].weatherElement[0].time[0].parameter.parameterValue,
+        pop: countyData[13].weatherElement[1].time[0].parameter.parameterName,
+        minT: countyData[13].weatherElement[2].time[0].parameter.parameterName,
+        maxT: countyData[13].weatherElement[4].time[0].parameter.parameterName,
+        ci: countyData[13].weatherElement[3].time[0].parameter.parameterName,
+        date: ((countyData[13].weatherElement[0].time[0].startTime).split(' ')[0]).split('-'),
+    };
+    taoyuanInfo.push(taoyuanData);
+    
+    const taoyuanWeather = function(taoyuanInfo){
+      let taoyuanStr = "";
+
+      for (idx in taoyuanInfo) {
+        const { county, wx, wxV, minT, maxT, pop, ci, date } = taoyuanInfo[idx];
+
+        taoyuanStr = `
+        <div class="wxPage1">
+            <div class="wxPageLocation">
+              <span><i class="fas fa-map-marker-alt"></i> ${county}</span>
+            </div>
+            <div class="wxPageImg">
+              <img src="images/weatherImg/${wxV}.svg" alt="${wx}">
+            </div>
+          </div>
+          <div class="wxPage2">
+            <div>
+              <span> ${date[1]}/${date[2]}</span>
+            </div>
+            <ul>
+              <li><span>${wx}</span></li>
+              <li><span><i class="fas fa-temperature-low"></i> ${minT} ~ ${maxT}</span><i>˚C</i></li>
+              <li><span><i class="fas fa-umbrella"></i> ${pop}</span><i>%</i></li>
+              <li><span>${ci}</span></li>
+            </ul>
+          </div>`;
+      };
+      $(".countyWxPage").empty();
+      $(".countyWxPage").html(taoyuanStr);
+    };
+    taoyuanWeather(taoyuanInfo);
   });
 });
 
-
+// click氣象小圖顯示氣象資訊
 $('.weatherData').on('click', 'img', function(e){
+  const id = $(e.target).parent('a').attr('name');
   
   $(".hidden").removeClass("hidden");
   $(".show").removeClass("show");
+  $(".click").removeClass("click");
+  $(".countyWeather").hide();
 
   $(e.target).addClass("hidden");
   $(e.target).siblings("div").addClass("show");
+  $(`#${id}`).parent("a").addClass("click");
+  $(".countyWeather").fadeIn("quick");
 });
 
+// click地圖顯示氣象資訊
+$('.twMap a').on('click', function(e){
+  
+  const id = $(e.target).parent('g').attr('id');
+
+  $(".hidden").removeClass("hidden");
+  $(".show").removeClass("show");
+
+  $(`.iconZone[name=${id}] img`).addClass("hidden");
+  $(`.iconZone[name=${id}] div`).addClass("show");
+
+  $(".click").removeClass("click");
+  $(e.target).parents("a").addClass("click");
+
+  $(".countyWeather").hide();
+  $(".countyWeather").fadeIn("quick");
+});
+
+// click氣象資訊換回氣象小圖
 $('.weatherData').on('click', 'div', function(e){
 
   $(".hidden").removeClass("hidden");
   $(".show").removeClass("show");
+  $(".click").removeClass("click");
 });
--- weather/townWeatherInfo.js
+++ weather/townWeatherInfo.js
@@ -1,38 +1,40 @@
-const townSelectData = [];
-const townWeatherData = [];
-const forecastData = [];
+const townSelectInfo = [];
+const townWeatherInfo = [];
+const forecastInfo = [];
 
 $('.searchBtn').on('click', function(){
-    if (townWeatherData.length === 0){
+    if (townWeatherInfo.length === 0){
         alert("請選擇縣市及鄉鎮！");
     };
 });
 
+// 選擇縣市
 $("#countySelect").on("change", function(){
     const countyValue = $(this).val();
-    townSelectData.length = 0;
-    townWeatherData.length = 0;
+    townSelectInfo.length = 0;
+    townWeatherInfo.length = 0;
     
-    
+    // 依縣市索取鄉鎮的氣象資料
     $.get(`https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-${countyValue}?Authorization=CWB-4D24F5BE-73F7-485A-94A0-AB7B5E86D535`,
-    function(data){        
-        const townInfo = data["records"].locations[0].location;
+    function(data){
+        console.log(data);        
+        const townData = data["records"].locations[0].location;
         
-        //將鄉鎮資料寫入townSelect
-        for (i = 0; i < townInfo.length; i++) {           
-            const townSelectInfo = {
+        //將鄉鎮地區寫入townSelect
+        for (i = 0; i < townData.length; i++) {           
+            const townSelectData = {
                 i: i,
-                town: townInfo[i].locationName,
+                town: townData[i].locationName,
             };
             
-            townSelectData.push(townSelectInfo);            
+            townSelectInfo.push(townSelectData);            
         };
 
-        const selectData = function(townSelectData){
+        const selectInfo = function(townSelectInfo){
             let selectStr = "";
 
-            for (idx in townSelectData) {
-                const {i, town} = townSelectData[idx];
+            for (idx in townSelectInfo) {
+                const {i, town} = townSelectInfo[idx];
                 selectStr += `
                 <option value="${i}">${town}</option>
                 `;                    
@@ -41,20 +43,18 @@ $("#countySelect").on("change", function(){
             $('#townSelect').html(`<option value disabled selected>選擇鄉鎮</option>`)
             $('#townSelect').append(selectStr);
         };
-        selectData(townSelectData);
+        selectInfo(townSelectInfo);
 
         //整理鄉鎮的氣象資料
         $('#townSelect').on('change', function(){
             const townValue = $(this).val();
 
-            const wxs = townInfo[townValue].weatherElement;
-            //console.log(wxs);
+            const wxs = townData[townValue].weatherElement;
 
-            //鄉鎮的當日氣象
-            
-            const townWeatherInfo = { 
+            //鄉鎮的當日氣象            
+            const townWeatherData = { 
                 county: data["records"].locations[0].locationsName,
-                town: townInfo[townValue].locationName,
+                town: townData[townValue].locationName,
                 time: (wxs[0].time[0].startTime).split(' ')[0],
                 wx: wxs[6].time[0].elementValue[0].value,
                 wxV: wxs[6].time[0].elementValue[1].value,
@@ -71,13 +71,13 @@ $("#countySelect").on("change", function(){
                 ws: wxs[4].time[0].elementValue[1].value,
                 ci: (wxs[10].time[0].elementValue[0].value).split('。')[3],
             };
-            townWeatherData.length = 0;
-            townWeatherData.push(townWeatherInfo);
+            townWeatherInfo.length = 0;
+            townWeatherInfo.push(townWeatherData);
 
             //鄉鎮的氣象預報
-            forecastData.length = 0;
+            forecastInfo.length = 0;
             for (i = 1; i < (wxs[0].time).length; i = i + 2){
-                const forecastInfo = {
+                const forecastData = {
                     time: (wxs[0].time[i].startTime).split(' ')[0],
                     wx: wxs[6].time[i].elementValue[0].value,
                     wxV: wxs[6].time[i].elementValue[1].value,
@@ -85,21 +85,22 @@ $("#countySelect").on("change", function(){
                     minT: wxs[8].time[i].elementValue[0].value,
                     maxT: wxs[12].time[i].elementValue[0].value,
                 };
-                forecastData.push(forecastInfo);
+                forecastInfo.push(forecastData);
             };
 
             // 點擊搜尋鈕
             $('.searchBtn').unbind('click').on('click', function(){
-                if (townWeatherData.length === 0){
+                if (townWeatherInfo.length === 0){
                     alert("請選擇縣市及鄉鎮！");
                 } else {
-                    const weatherData = function(townWeatherData){
+                    // 把資料寫入weatherWrap和weatherText
+                    const weatherInfo = function(townWeatherInfo){
                         let wrapStr = "";
                         let textStr = "";
                         
                         
-                        for (idx in townWeatherData) {
-                            const {county, town, time, wx, wxV, pop, rh, minT, maxT, minAT, maxAT, uvi, uviV, wd, ws, ci} = townWeatherData[idx];
+                        for (idx in townWeatherInfo) {
+                            const {county, town, time, wx, wxV, pop, rh, minT, maxT, minAT, maxAT, uvi, uviV, wd, ws, ci} = townWeatherInfo[idx];
         
                             const date = time.split('-');
         
@@ -108,12 +109,12 @@ $("#countySelect").on("change", function(){
                               <img src="images/weatherImg/${wxV}.svg" alt="${wx}">
                             </div>
                             <div class="location">
-                                <span><i class="fas fa-map-marker-alt"></i>${county} ${town}</span>
+                                <span><i class="fas fa-map-marker-alt"></i> ${county} ${town}</span>
+                            </div>
+                            <div class="ci">
+                              <span>${ci}</span>
                             </div>
                             <div class="weatherIcon">
-                              <td class="date">
-                                <span>${date[1]}/${date[2]}</span>
-                              </td>
                               <button class="temperature">
                                 <i class="fas fa-temperature-low"></i>
                                 <span>${minT}~${maxT}˚C</span>
@@ -122,17 +123,15 @@ $("#countySelect").on("change", function(){
                                 <i class="fas fa-umbrella"></i>
                                 <span>${pop}<i>%</i></span>
                               </button>
-                              <button class="wind">
-                                <i class="fas fa-wind"></i>
-                                <span>${wd}(風速${ws}級)</span>
-                              </button>
                               <button class="uvi">
                                 <i class="far fa-sun"></i>
                                 <span>${uvi}</span>
                               </button>
-                            </div>
-                            <div class="ci">
-                              <span>${ci}</span>
+                              <button class="wind">
+                                <i class="fas fa-wind"></i>
+                                <span>${wd}</span>
+                                <span>(風速${ws}級)</span>
+                              </button>
                             </div>
                             `
         
@@ -159,36 +158,190 @@ $("#countySelect").on("change", function(){
         
                         $(".weatherText").empty();
                         $(".weatherText").html(textStr);
+
+                        $(".weather").hide();
+                        $(".weather").fadeIn("quick");
                     };
-                    weatherData(townWeatherData);
-        
-                    const forecast = function(forecastData){
+                    weatherInfo(townWeatherInfo);
+                    
+                    // 把氣象預報寫入weekWeather
+                    const forecast = function(forecastInfo){
                         let forecastStr = "";
                                 
-                        for (idx in forecastData) {
-                            const { time, wx, wxV, pop, minT, maxT } = forecastData[idx];
+                        for (idx in forecastInfo) {
+                            const { time, wx, wxV, pop, minT, maxT } = forecastInfo[idx];
         
                             const date = time.split('-');
         
                             forecastStr += `
-                            <div class="dayInfo">
-                                <p>${date[1]}/${date[2]}</p>
-                            </div>
                             <div class="dayWeather">
-                                <div>
-                                    <span>${wx}</span>
-                                    <span><i class="fas fa-temperature-low"></i> ${minT}~${maxT}˚C</span>
-                                    <span><i class="fas fa-umbrella"></i>${pop}<i>%</i></span>
+                                <div class="forecastInfo">
+                                    <li class="forecastDate"><span> ${date[1]}/${date[2]}<span>
+                                    <li><span>${wx}</span></li>
+                                    <li><span><i class="fas fa-temperature-low"></i> ${minT}~${maxT}˚C</span></li>
+                                    <li><span><i class="fas fa-umbrella"></i>${pop}<i>%</i></span></li>
                                 </div>
                                 <img src="images/weatherImg/${wxV}.svg" alt="${wx}">
+                                <div class="timeInfo">
+                                    <p>${date[1]}/${date[2]}</p>
+                                </div>
                             </div>`;
                         };
                         $(".weekWeather").empty();
-                        $(".weekWeather").html(forecastStr);                
+                        $(".weekWeather").html(forecastStr)
+
+                        $(".weekWeather").hide();
+                        $(".weekWeather").fadeIn("quick");
                     };
-                    forecast(forecastData);  
+                    forecast(forecastInfo);  
                 };             
             });        
         });
     });
 });
+
+// 預設氣象(新北市新莊區)
+
+const xinzhuangWxInfo = [];
+const xinzhuangForecast = [];
+
+$.get(`https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-071?Authorization=CWB-4D24F5BE-73F7-485A-94A0-AB7B5E86D535`,
+    function(data){        
+        const xinzhuangData = data["records"].locations[0].location[5];
+        const wxs = xinzhuangData.weatherElement;
+        // 新莊區當日氣象
+        const xinzhuangWxData = { 
+            county: data["records"].locations[0].locationsName,
+            town: xinzhuangData.locationName,
+            time: (wxs[0].time[0].startTime).split(' ')[0],
+            wx: wxs[6].time[0].elementValue[0].value,
+            wxV: wxs[6].time[0].elementValue[1].value,
+            pop: wxs[0].time[0].elementValue[0].value,
+            rh: wxs[2].time[0].elementValue[0].value,
+            minT: wxs[8].time[0].elementValue[0].value,
+            maxT: wxs[12].time[0].elementValue[0].value,
+            minAT: wxs[11].time[0].elementValue[0].value,
+            maxAT: wxs[5].time[0].elementValue[0].value,
+            T: wxs[1].time[0].elementValue[0].value,
+            uvi: wxs[9].time[0].elementValue[1].value,
+            uviV: wxs[9].time[0].elementValue[0].value,
+            wd: wxs[13].time[0].elementValue[0].value,
+            ws: wxs[4].time[0].elementValue[1].value,
+            ci: (wxs[10].time[0].elementValue[0].value).split('。')[3],
+        };
+        xinzhuangWxInfo.push(xinzhuangWxData);
+
+        // 新莊區氣象預報
+        for (i = 1; i < (wxs[0].time).length; i = i + 2){
+            const forecastData = {
+                time: (wxs[0].time[i].startTime).split(' ')[0],
+                wx: wxs[6].time[i].elementValue[0].value,
+                wxV: wxs[6].time[i].elementValue[1].value,
+                pop: wxs[0].time[i].elementValue[0].value,
+                minT: wxs[8].time[i].elementValue[0].value,
+                maxT: wxs[12].time[i].elementValue[0].value,
+            };
+            xinzhuangForecast.push(forecastData);
+        };
+
+        // 把新莊氣象資料寫入weatherWrap和weatherText
+        const weatherInfo = function(xinzhuangWxInfo){
+            let wrapStr = "";
+            let textStr = "";
+            
+            
+            for (idx in xinzhuangWxInfo) {
+                const {county, town, time, wx, wxV, pop, rh, minT, maxT, minAT, maxAT, uvi, uviV, wd, ws, ci} = xinzhuangWxInfo[idx];
+
+                const date = time.split('-');
+
+                wrapStr = `
+                <div class="weatherImg">
+                  <img src="images/weatherImg/${wxV}.svg" alt="${wx}">
+                </div>
+                <div class="location">
+                    <span><i class="fas fa-map-marker-alt"></i> ${county} ${town}</span>
+                </div>
+                <div class="ci">
+                  <span>${ci}</span>
+                </div>
+                <div class="weatherIcon">
+                  <button class="temperature">
+                    <i class="fas fa-temperature-low"></i>
+                    <span>${minT}~${maxT}˚C</span>
+                  </button>
+                  <button class="pop">
+                    <i class="fas fa-umbrella"></i>
+                    <span>${pop}<i>%</i></span>
+                  </button>
+                  <button class="uvi">
+                    <i class="far fa-sun"></i>
+                    <span>${uvi}</span>
+                  </button>
+                  <button class="wind">
+                    <i class="fas fa-wind"></i>
+                    <span>${wd}</span>
+                    <span>(風速${ws}級)</span>
+                  </button>
+                </div>
+                `
+
+                textStr = `
+                <div class="currentDate">
+                    <h1>${date[0]} / ${date[1]} / ${date[2]}</h1>
+                </div>
+                <div class="weatherText">
+                    <ul>
+                        <li>天氣現象：<span>${wx}</span></li>
+                        <li>濕度：<span>${rh}</span><i>%</i></li>
+                        <li>降雨機率：<span>${pop}</span><i>%</i></li>
+                        <li>溫度：<span>${minT}～${maxT}</span><i>˚C</i></li>
+                        <li>體感溫度：<span>${minAT}～${maxAT}</span><i>˚C</i></li>
+                        <li>紫外線指數：<span>${uviV} (${uvi})</span></li>
+                        <li>風向：<span>${wd}(風速${ws}級)</span></li>
+                        <li>舒適度：<span>${ci}</span></li>
+                    </ul>
+                </div>
+                `
+            }
+            $(".weatherWrap").empty();
+            $(".weatherWrap").html(wrapStr);
+
+            $(".weatherText").empty();
+            $(".weatherText").html(textStr);
+        };
+        weatherInfo(xinzhuangWxInfo);
+        
+        // 把新莊氣象預報寫入weekWeather
+        const forecast = function(xinzhuangForecast){
+            let forecastStr = "";
+                    
+            for (idx in xinzhuangForecast) {
+                const { time, wx, wxV, pop, minT, maxT } = xinzhuangForecast[idx];
+
+                const date = time.split('-');
+
+                forecastStr += `
+                <div class="dayWeather">
+                    <div class="forecastInfo">
+                        <li class="forecastDate"><span> ${date[1]}/${date[2]}<span>
+                        <li><span>${wx}</span></li>
+                        <li><span><i class="fas fa-temperature-low"></i> ${minT}~${maxT}˚C</span></li>
+                        <li><span><i class="fas fa-umbrella"></i>${pop}<i>%</i></span></li>
+                    </div>
+                    <img src="images/weatherImg/${wxV}.svg" alt="${wx}">
+                    <div class="timeInfo">
+                        <p>${date[1]}/${date[2]}</p>
+                    </div>
+                </div>`;
+            };
+            $(".weekWeather").empty();
+            $(".weekWeather").html(forecastStr)
+
+            $(".weekWeather").hide();
+            $(".weekWeather").fadeIn("quick");
+        };
+        forecast(xinzhuangForecast);
+    }
+);
+
